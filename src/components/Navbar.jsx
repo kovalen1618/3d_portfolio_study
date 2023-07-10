ded738621830dd4eaecf44bc17f14a26bc1e578d
kovalen1618 <kovalen1.618@gmail.com>
2023-07-10 14:25:54 -0500

    Created Hero component w/ Three.js & FramerMotion
    3D model is successfully rendering within Hero
    component through a combination of Three.js, Fiber,
    and drei. An animated slider to lead users to About
    section was implemented using FramerMotion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         `${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`
       }
     >
-      <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
           className="flex items-center gap-2"
